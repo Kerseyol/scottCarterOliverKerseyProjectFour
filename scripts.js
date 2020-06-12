f11b0455661fa178a9af58e541ad50f4d2642aa9
Scott Carter <scottandrewcarter1@hotmail.com>
2020-06-12 00:26:15 -0400

    set up scss folders and styled some checkboxes.

---
+++ scripts.js
@@ -0,0 +1,57 @@
+$(function(){
+
+    //button on click function event
+
+    const button = {};
+
+    // $('button').one('click', function(event){
+    //     button.first = event.target.innerText
+    // })
+
+    // const generate = function(query){
+    $('checkbox').one('click', function(event){
+
+        button.first = event.target.innerText
+                        // second: event.target.innerText
+        })
+
+        const button2 = {};
+        console.log(button);
+
+        $('.gifContainer').empty();
+        // event.target.innerText;
+        
+
+        $.ajax({
+            url:'https://api.giphy.com/v1/gifs/random',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                api_key: 'uOoh1YcEJMW5RZkHeRFcIl5PkkMudbnv',
+                tag: button.first
+            }
+            }).then(function(e) {
+            const embedUrl = e.data.embed_url;
+            // const url = e.data.url;
+
+
+//Append may still be used, but we have to clear the gifs  COMPLETED
+
+//We need to fit the gif to container, remove black bars, additional PARTIALLY COMPLETE
+
+//possibly put div on top of bars to remove visibility
+
+//default image of eyeball as default monitor image COMPLETED
+
+//look up embed stylings COMPLETED
+
+//stretch goal: get rid of hover feature if possible
+
+//add combination tags - keep the buttons
+
+            $('.gifContainer').append(`<div style="width:400px;height:400px;padding-bottom:0%;position:relative;"><iframe src="${embedUrl}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe></div>`);
+        });
+        // <p><a href="${url}">via GIPHY</a></p>
+        // allowFullScreen
+    })    
+// }
