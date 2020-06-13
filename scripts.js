d13f545254ee137eb7da9a3ba2661d06c9158ab2
Scott Carter <scottandrewcarter1@hotmail.com>
2020-06-13 19:50:17 -0400

    namespaced the js file, reworked the code a bit, made it accept and store 2 inputs.

--- scripts.js
+++ scripts.js
@@ -1,57 +1,89 @@
-$(function(){
 
-    //button on click function event
+const moodApp = {};
 
-    const button = {};
+const finalSelection = {
+    primaryMood: '',
+    secondaryMood: ''
+}
+let clicks = 0;
 
-    // $('button').one('click', function(event){
-    //     button.first = event.target.innerText
-    // })
-
-    // const generate = function(query){
-    $('checkbox').one('click', function(event){
-
-        button.first = event.target.innerText
-                        // second: event.target.innerText
-        })
-
-        const button2 = {};
-        console.log(button);
-
-        $('.gifContainer').empty();
-        // event.target.innerText;
+moodApp.checkboxValue = function (){
+    $('input[type=checkbox]').on('click', function(){
         
+        if (clicks === 0){
+            if(this.checked) {
+                // audTag[1].currentTime = 0;
+                // audTag[1].play();
+                finalSelection.primaryMood = this.value;             
+            } else if (!this.checked) {
+                // audTag[1].currentTime = 0
+                // audTag[1].play();
+                delete finalSelection.primaryMood;
+            }
+        } else {
+            if(this.checked) {
+                // audTag[1].currentTime = 0;
+                // audTag[1].play();
+                finalSelection.secondaryMood = this.value;             
+            } else if (!this.checked) {
+                // audTag[1].currentTime = 0
+                // audTag[1].play();
+                delete finalSelection.secondaryMood;
+            }
+        }
+        clicks++;
+    })
+}
 
+moodApp.activate = function (){
+    $('.button').on('click', function(){
+        $('.gifContainer').empty();
         $.ajax({
             url:'https://api.giphy.com/v1/gifs/random',
             method: 'GET',
             dataType: 'json',
             data: {
                 api_key: 'uOoh1YcEJMW5RZkHeRFcIl5PkkMudbnv',
-                tag: button.first
+                tag: `${finalSelection.primaryMood} ${finalSelection.secondaryMood}`
             }
-            }).then(function(e) {
-            const embedUrl = e.data.embed_url;
-            // const url = e.data.url;
+        }).then(function(e){
+                const embedUrl = e.data.embed_url;
+                $('.gifContainer').append(`<div style="width:400px;height:400px;padding-bottom:0%;position:relative;"><iframe src="${embedUrl}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe></div>`)
+        })
+        // audTag[2].play();
+        
+        setTimeout(moodApp.switchReturn, 8000);
+        })
+        
+    }
+
+moodApp.switchReturn = function(){
+    $('.gifContainer').empty(),
+    ($('.gifContainer').append(`<div style="width:400px;height:400px;padding-bottom:0%;position:relative;"><iframe src="https://giphy.com/embed/1qpQwleotpxXG" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe></div>`));
+    
+    const audTag = document.getElementsByTagName("audio");
+    // audTag[0].play(); 
+}
+
+moodApp.reset = function(){
+    $('.resetButton').on('click', function(){
+        // audTag[3].play();
+        $('input[type=checkbox]').each(function() { 
+        this.checked = false; 
+        finalSelection.primaryMood = '';
+        }); 
+    })
+}   
 
 
-//Append may still be used, but we have to clear the gifs  COMPLETED
 
-//We need to fit the gif to container, remove black bars, additional PARTIALLY COMPLETE
+moodApp.init = () => {
+    moodApp.checkboxValue();
+    moodApp.activate();
+    moodApp.switchReturn();
+    moodApp.reset();
+}
 
-//possibly put div on top of bars to remove visibility
-
-//default image of eyeball as default monitor image COMPLETED
-
-//look up embed stylings COMPLETED
-
-//stretch goal: get rid of hover feature if possible
-
-//add combination tags - keep the buttons
-
-            $('.gifContainer').append(`<div style="width:400px;height:400px;padding-bottom:0%;position:relative;"><iframe src="${embedUrl}" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed"></iframe></div>`);
-        });
-        // <p><a href="${url}">via GIPHY</a></p>
-        // allowFullScreen
-    })    
-// }-
+$(function(){
+    moodApp.init();
+})
